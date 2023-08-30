77c0f3fffaae215f3ec2ef206a6f793d4cf48427
ruo980 <3040357205@qq.com>
2023-08-30 18:31:57 +0800

    Gin项目学习——swagger处理函数编写注释

--- routers/user_router.go
+++ routers/user_router.go
@@ -5,15 +5,16 @@
 package routers
 
 import (
+	"awesomeProject/api"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func InitUserRouter() {
 	RegistRoute(func(rgPublic *gin.RouterGroup, rgAuth *gin.RouterGroup) {
-		rgPublic.POST("/login", func(context *gin.Context) {
-			context.AbortWithStatusJSON(http.StatusOK, gin.H{"msg": "Login Success"})
-		})
+		userApi := api.NewUserApi()
+		rgPublicUser := rgPublic.Group("user")
+		rgPublicUser.POST("/login", userApi.Login)
 		rgAuthUser := rgAuth.Group("user")
 		rgAuthUser.GET("", func(context *gin.Context) {
 			context.AbortWithStatusJSON(http.StatusOK, gin.H{
